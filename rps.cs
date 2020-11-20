cad36589609015b890016b126198c2ad8882f17c
J Mosforth <jmosforth65@gmail.com>
2020-11-20 18:28:53 +0000

    i made sure everything worked and tested it.

--- rps.cs
+++ rps.cs
@@ -4,46 +4,43 @@ namespace L5_4_Cumulative_Annual_Profit
 {
     class Program
     {
-		public static int user = 0;
-		public static int cpu = 0;
+		public float use = 0;
+		public float cpu = 0;
 		
         static void Main(string[] args)
         {
 			Random rnd = new Random();
-			
-			while (cpu = 0)
-			{
-				int cpu = rnd.Next(1, 4);
-			}
+			float cpu = rnd.Next(1,4);
 			
 			Console.WriteLine("Choose either rock (enter 1), paper (enter 2) or scissors (enter 3): ");
-			Console.ReadLine(user);
+			string use = Console.ReadLine();
+			float user = float.Parse(use);
 			
-			if (comp = user)
+			if (cpu == 1 && user == 1 || cpu == 2 && user == 2 || cpu == 3 && user == 3)
 			{
 				Console.WriteLine("You tied! ");
 			}
-			else if (comp = "1" && user = "2")
+			else if (cpu == 1 && user == 2)
 			{
 				Console.WriteLine("You win! ");
 			}
-			else if (comp = "1" && user = "3")
+			else if (cpu == 1 && user == 3)
 			{
 				Console.WriteLine("You lose. ");
 			}
-			else if (comp = "2" && user = "1")
+			else if (cpu == 2 && user == 1)
 			{
 				Console.WriteLine("You lose. ");
 			}
-			else if (comp = "2" && user = "3")
+			else if (cpu == 2 && user == 3)
 			{
 				Console.WriteLine("You win! ");
 			}
-			else if (comp = "3" && user = "1")
+			else if (cpu == 3 && user == 1)
 			{
 				Console.WriteLine("You win! ");
 			}
-			else if (comp = "3" && user = "2")
+			else if (cpu == 3 && user == 2)
 			{
 				Console.WriteLine("You lose. ");
 			}
