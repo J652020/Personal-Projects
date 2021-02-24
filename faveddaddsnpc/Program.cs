9b37800ddcf26ba94228e1a4a1e6f2fde3ae641d
J MOSFORTH <S.MOSFORTH-2020@hull.ac.uk>
2021-02-24 22:41:11 +0000

    fave dd npc

---
+++ faveddaddsnpc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace faveddaddsnpc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which NPC from Dream Daddy is your favourite? ");
+            Console.ReadLine();
+            Console.WriteLine("That's understandable, they are a cute character! ");
+
+            Console.WriteLine("Press any key to exit. ");
+            Console.ReadKey();
+        }
+    }
+}
