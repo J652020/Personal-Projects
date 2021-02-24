10ef914567eee2e37f8332acebe33f5b7361be96
J MOSFORTH <S.MOSFORTH-2020@hull.ac.uk>
2021-02-24 23:02:10 +0000

    made a program that asks you what your favourite coding language is.

---
+++ favecodinglang/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace favecodinglang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favourite coding language?");
+            Console.ReadLine();
+            Console.WriteLine("I like python the most but I am the best at C#. ");
+
+            Console.WriteLine("Press any key to exit. ");
+            Console.ReadKey();
+        }
+    }
+}
